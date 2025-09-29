6a082c98de723dc160c0a383aebaa814859ab617
Jack_Chiu <qqwer12313@gmail.com>
2025-09-29 22:05:55 +0800

    關於我們與最新消息頁面實作_20250929

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -1,3 +1,5 @@
 import { Page } from "./components/Page.js";
+import { PageBanner } from "./components/PageBanner.js";
 
 Page.loader("contact");
+PageBanner.loader("#page-banner-endpoint", "聯絡我們", "與我們聯絡，讓我們為您提供最佳的服務。");
